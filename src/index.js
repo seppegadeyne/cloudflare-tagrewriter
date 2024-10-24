6a998a31cdb72e0ea961d06c1ff151e0718295cb
Seppe Gadeyne <seppe@fushia.be>
2024-10-24 13:07:36 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ export default {
   async fetch(request) {
     class TagRewriter {
       element(element) {
-        element.setAttribute('data-modified', new Date().toUTCString())
-        element.tagName = 'template' 
-        console.log(`Found script v2: ${element.tagName} / ${element.src}`)
+        element.setAttribute('data-modified', new Date())
+        if (element.src) element.tagName = 'template' 
+        console.log(`Found script v3: ${element.tagName} / ${element.src}`)
       }
     }
 
@@ -14,10 +14,8 @@ export default {
     const contentType = response.headers.get('Content-Type') || ''
 
     if (contentType.includes('text/html')) {
-      console.log('Rewriting HTML v2')
       return rewriter.transform(response)
     } else {
-      console.log('Skipping HTML rewrite v2')
       return response
     }
   },
