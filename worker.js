65510180cae03d79ca43d1fb0f95b6d1da65a667
Seppe Gadeyne <seppe@fushia.be>
2024-10-24 12:33:07 +0200

    update

---
+++ worker.js
@@ -0,0 +1,31 @@
+export default {
+	async fetch(request) {
+		class ScriptRewriter {
+			constructor(src) {
+				this.src = src
+			}
+
+			element(element) {
+				const src = element.getAttribute(this.src)
+
+				if (src) {
+					element.tagName = 'template'
+					console.log(`Found script: ${src}`)
+				}
+			}
+		}
+
+		const rewriter = new HTMLRewriter().on('script', new ScriptRewriter('src'))
+
+		const response = await fetch(request)
+		const contentType = response.headers.get('Content-Type') || ''
+
+		if (contentType.includes('text/html')) {
+			console.log('Rewriting HTML')
+			return rewriter.transform(response)
+		} else {
+			console.log('Skipping HTML rewrite')
+			return response
+		}
+	}
+}
