9f3a756352d61defb8e77f62172c0ef93b079244
Seppe Gadeyne <seppe@fushia.be>
2024-10-24 16:29:59 +0200

    update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export default {
     const response = await fetch(request),
       contentType = response.headers.get('Content-Type') || ''
     
-    console.log('Pahtname:', request.url)
+    console.log('Pahtname: ', request.url)
 
     if (contentType.includes('text/html')) {
       return rewriter.transform(response)
