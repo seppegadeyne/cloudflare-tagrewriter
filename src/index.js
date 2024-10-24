628797da216293101c84768e434d6b07b93e50e7
Seppe Gadeyne <seppe@fushia.be>
2024-10-24 13:18:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 export default {
   async fetch(request) {
     class TagRewriter {
+      constructor(src) {
+        this.src = src
+      }
+
       element(element) {
+        const src = element.getAttribute(this.src)
+
         element.setAttribute('data-modified', new Date())
-        if (element.src) element.tagName = 'template' 
-        console.log(`Found script v4: ${element.tagName} / ${element.src}`)
+        if (src) element.tagName = 'template' 
+        console.log(`Found script v4: ${element.tagName} / ${src}`)
       }
     }
 
-    const rewriter = new HTMLRewriter().on('script', new TagRewriter())
+    const rewriter = new HTMLRewriter().on('script', new TagRewriter('src'))
 
     const response = await fetch(request)
     const contentType = response.headers.get('Content-Type') || ''
